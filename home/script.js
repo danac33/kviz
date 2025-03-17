e5920990480d7f760063e9e107c60ee03767b62b
Danin <danac.opasni@gmail.com>
2025-03-17 23:08:42 +0100

    Add files via upload

--- home/script.js
+++
@@ -1,5 +0,0 @@
-
-function singIn() {
-    window.location = "http://127.0.0.1:5500/singUp/index.html#"
-    alert ("hi")
-}
---
+++ questions/type1/script.js
@@ -0,0 +1,7 @@
+const Qnum = document.getElementById('Qnum');
+const Answer1 = document.getElementById('A1');
+const Answer2 = document.getElementById('A2');
+const Answer3 = document.getElementById('A3');
+const Answer4 = document.getElementById('A4');
+const QuestionText = document.getElementById('Questiontext');
+const Points = document.getElementById('Points');[]
