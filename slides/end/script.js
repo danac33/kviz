b585418294de0469f574900ea5888eea26346f0a
Danin <danac.opasni@gmail.com>
2025-05-02 16:10:19 +0200

    Delete slides directory

--- slides/end/script.js
+++
@@ -1,9 +0,0 @@
-const p = localStorage.getItem("points")
-const point = document.getElementById("points")
-if(p == 1) {
-    point.innerHTML = `${p} bod`;
-} else if(p > 4 || p == 0){
-    point.innerHTML = `${p} bodova`;
-}else  {
-point.innerHTML = `${p} boda`;
-}
